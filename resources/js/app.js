61f80deba0cfd7de397cbae30472ea0cd1e3582d
Ali Raza Saleem <thealichemist1@gmail.com>
2025-03-11 11:57:47 +0500

    fix-style

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,56 +1,56 @@
-document.addEventListener('alpine:init', () => {
-  Alpine.data('root', () => ({
+document.addEventListener("alpine:init", () => {
+  Alpine.data("root", () => ({
     init() {
-      this.vscode = acquireVsCodeApi()
-      document.addEventListener('keydown', (event) =>
+      this.vscode = acquireVsCodeApi();
+      document.addEventListener("keydown", (event) =>
         this.handleKeyboardShortcuts(event),
-      )
-      window.addEventListener('message', (event) =>
+      );
+      window.addEventListener("message", (event) =>
         this.handleVSCodeMessages(event),
-      )
+      );
 
-      this.showSearchBar = false
+      this.showSearchBar = false;
 
-      this.$watch('outputs', () => {
+      this.$watch("outputs", () => {
         this.$nextTick(() => {
-          this.searchText = ''
+          this.searchText = "";
 
-          const oldOutputElementsCount = this.outputElements.length
+          const oldOutputElementsCount = this.outputElements.length;
           this.outputElements = Array.from(
-            this.$refs.outputContainer.querySelectorAll('.output-element'),
-          )
+            this.$refs.outputContainer.querySelectorAll(".output-element"),
+          );
 
           if (
             !this.outputElements.length ||
             oldOutputElementsCount === this.outputElements.length
           ) {
-            return
+            return;
           }
 
-          const isFirstElement = this.outputElements.length === 1
-          let lastElement = this.outputElements[this.outputElements.length - 1]
+          const isFirstElement = this.outputElements.length === 1;
+          let lastElement = this.outputElements[this.outputElements.length - 1];
 
-          const lastOutput = this.outputs[this.outputs.length - 1]
-          lastOutput['element'] = lastElement
+          const lastOutput = this.outputs[this.outputs.length - 1];
+          lastOutput["element"] = lastElement;
 
-          const highlightLanguage = lastOutput.isError ? 'accessLog' : 'php'
+          const highlightLanguage = lastOutput.isError ? "accessLog" : "php";
 
           window.hljs.highlightElement(
-            lastElement.querySelector('code'),
+            lastElement.querySelector("code"),
             { language: highlightLanguage },
-          )
+          );
 
           // We don't want to scroll to the first element
           if (isFirstElement) {
-            return
+            return;
           }
 
           setTimeout(() => {
-            lastElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            lastElement.scrollIntoView({ behavior: "smooth", block: "start" });
           }, 50);
 
-        })
-      })
+        });
+      });
     },
 
     vscode: null,
@@ -60,133 +60,133 @@ document.addEventListener('alpine:init', () => {
     outputs: [],
     outputElements: {},
     showDetailLogs: false,
-    searchText: '',
+    searchText: "",
 
     collectOutputElements(el, index) {
       this.$nextTick(() => {
-        this.outputElements[index] = el
-      })
+        this.outputElements[index] = el;
+      });
     },
 
     clearOutput() {
-      this.outputs = []
-      this.outputElements = []
-      this.showSearchBar = false
+      this.outputs = [];
+      this.outputElements = [];
+      this.showSearchBar = false;
     },
 
     stopCodeExecution() {
-      this.vscode.postMessage({ command: 'stopExecution' })
-      this.stopCodeExecutionButtonVisibility = false
+      this.vscode.postMessage({ command: "stopExecution" });
+      this.stopCodeExecutionButtonVisibility = false;
     },
 
     handleVSCodeMessages(event) {
-      const message = event.data
+      const message = event.data;
 
-      this.stopCodeExecutionButtonVisibility = message.isRunning
+      this.stopCodeExecutionButtonVisibility = message.isRunning;
 
-      if (message.command === 'scriptStarted') {
-        this.stopCodeExecutionButtonVisibility = true
+      if (message.command === "scriptStarted") {
+        this.stopCodeExecutionButtonVisibility = true;
       }
-      if (message.command === 'scriptKilled') {
-        this.stopCodeExecutionButtonVisibility = false
+      if (message.command === "scriptKilled") {
+        this.stopCodeExecutionButtonVisibility = false;
       }
-      if (message.command === 'updateOutput') {
+      if (message.command === "updateOutput") {
         this.updateOutput(
           message.content,
           message.isError,
           message.appendOutput,
-        )
+        );
       }
-      if (message.command === 'clearOutput') {
-        this.clearOutput()
+      if (message.command === "clearOutput") {
+        this.clearOutput();
       }
-      if (message.command === 'focusSearchBar') {
-        this.$refs.searchInput.focus()
+      if (message.command === "focusSearchBar") {
+        this.$refs.searchInput.focus();
       }
     },
 
     handleKeyboardShortcuts(event) {
       if (event.ctrlKey && event.altKey) {
-        event.preventDefault()
-        const key = event.key.toLowerCase()
-        if (key === 'c') {
-          this.clearOutput()
+        event.preventDefault();
+        const key = event.key.toLowerCase();
+        if (key === "c") {
+          this.clearOutput();
         }
-        if (key === 'f') {
-          this.$refs.searchInput.focus()
+        if (key === "f") {
+          this.$refs.searchInput.focus();
         }
       }
     },
 
     updateOutput(content, isError, appendOutput) {
       if (!appendOutput) {
-        this.clearOutput()
+        this.clearOutput();
       }
 
       const output = {
         content: content,
         isError: isError,
         appendOutput: appendOutput,
-      }
+      };
 
-      this.showSearchBar = true
+      this.showSearchBar = true;
 
       // IMPORTANT: We wait for outputs to clear in next DOM update
       this.$nextTick(() => {
-        this.outputs.push(output)
-      })
+        this.outputs.push(output);
+      });
     },
 
     highlightSearchedText() {
 
-      const codeBlocks = this.$refs.outputContainer.querySelectorAll('pre code')
+      const codeBlocks = this.$refs.outputContainer.querySelectorAll("pre code");
       codeBlocks.forEach((code) => {
-        const instance = new Mark(code)
+        const instance = new Mark(code);
         instance.unmark({
           done: () => {
             instance.mark(this.searchText, {
               separateWordSearch: false,
-              className: 'highlight',
-            })
+              className: "highlight",
+            });
           },
-        })
-      })
+        });
+      });
     },
 
     highlightLog(output) {
-      if (output['logHighlighted']) {
-        return
+      if (output["logHighlighted"]) {
+        return;
       }
 
       this.$nextTick(() => {
-        const code = output.element.querySelector('.log-text')
-        window.hljs.highlightElement(code, { language: 'accessLog' })
+        const code = output.element.querySelector(".log-text");
+        window.hljs.highlightElement(code, { language: "accessLog" });
 
-        this.output['logHighlighted'] = true
-      })
+        this.output["logHighlighted"] = true;
+      });
     },
 
     copyOutput(output) {
-      const logText = output.content
+      const logText = output.content;
 
       if (!logText) {
-        console.warn('No text to copy')
-        return
+        console.warn("No text to copy");
+        return;
       }
 
       navigator.clipboard
         .writeText(logText)
         .then(() => {
-          output.outputCopied = true
+          output.outputCopied = true;
 
           // Revert after 1.5 seconds
           setTimeout(() => {
-            output.outputCopied = false
-          }, 1500)
+            output.outputCopied = false;
+          }, 1500);
         })
         .catch((err) => {
-          console.error('Clipboard API failed:', err)
-        })
+          console.error("Clipboard API failed:", err);
+        });
     },
-  }))
-})
+  }));
+});
